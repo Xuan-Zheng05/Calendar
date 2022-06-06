de7689d5cffe1fa778ea531beb955df5281578ae
Xuan Zheng <eagledude200@gmail.com>
2022-06-06 18:56:42 -0400

    work

--- app/src/main/java/com/firstapp/calendar/MainActivity.java
+++ app/src/main/java/com/firstapp/calendar/MainActivity.java
@@ -3,19 +3,43 @@ package com.firstapp.calendar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView username;
     private TextView password;
+    private Button submit;
+    private Button new_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialized variables
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
+        submit = findViewById(R.id.submit);
+        new_user = findViewById(R.id.new_user);
+
+        // button press for Submission, checks username and password
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // button press for New User?
+        new_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     public TextView getUser() {
