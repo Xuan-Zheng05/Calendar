ddd5bf458b66c3b5ef5af9b1b641de18ea9b59e3
Xuan Zheng <eagledude200@gmail.com>
2022-06-06 22:50:43 -0400

    added temp code for getting current date

--- app/src/main/java/com/calendar/CalendarActivity.java
+++ app/src/main/java/com/calendar/CalendarActivity.java
@@ -3,6 +3,8 @@ package com.calendar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -11,4 +13,13 @@ public class CalendarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
     }
+
+    /* temp code for finding current date
+
+    Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+
+    int currentYear = calendar.get(Calendar.YEAR);
+    int currentMonth = calendar.get(Calendar.MONTH) + 1;
+    int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
+    */
 }
--- app/src/main/java/com/calendar/MainActivity.java
+++ app/src/main/java/com/calendar/MainActivity.java
@@ -7,8 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import java.util.Calendar;
+import java.util.TimeZone;
+
 
 public class MainActivity extends AppCompatActivity {
 
