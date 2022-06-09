c57ab10ff6edf5ddbc7d3efdef33c94dcfd963c5
Xuan Zheng <eagledude200@gmail.com>
2022-06-09 01:28:52 -0400

    changes

--- app/src/main/java/com/calendar/MainActivity.java
+++ app/src/main/java/com/calendar/MainActivity.java
@@ -3,6 +3,7 @@ package com.calendar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +24,13 @@ public class MainActivity extends AppCompatActivity {
         password = findViewById(R.id.password);
         Button log_in = findViewById(R.id.log_in);
         Button new_user = findViewById(R.id.new_user);
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("aaa", "abc");
+        editor.apply();
+        String temp = sharedPreferences.getString("aaa", "");
+
 
         // button press for Submission, checks username and password
         // if correct, switch to calendar view
@@ -57,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // method to switch to calendar page
+    // also saves the username to transfer to CalendarActivity
     private void switchToCalendar() {
         Intent switchActivityIntent = new Intent(this, CalendarActivity.class);
         switchActivityIntent.putExtra("name", username.getText().toString());
--- app/src/main/java/com/calendar/RegistrationActivity.java
+++ app/src/main/java/com/calendar/RegistrationActivity.java
@@ -2,13 +2,21 @@ package com.calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    String temp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        temp = sharedPreferences.getString("aaa", "");
+        System.out.println(temp);
+        Log.d("abc", temp);
     }
 }
